bb48531a8569575c3d79d2d99170d9483f702a07
Yeah-metcha-Someday <itowokashigenari.jobjobworking@gmail.com>
2024-04-22 20:40:18 +0900

    feat:Userモデルを実装した

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,29 @@
 RSpec.describe User, type: :model do
-  let(:user) { build(:user) }
-  example 'userが有効である' do
-    expect(user.valid?).to be true
+  context 'emailとpasswordがそれぞれ有効な値であるとき' do
+    let(:user) { build(:user) }
+    example 'userが有効である' do
+      expect(user.valid?).to be true
+    end
+
+    example 'errorsが空である' do
+      user.valid?
+      expect(user.errors.full_messages).to eq([])
+    end
   end
 
-  example 'errorsが空である' do
-    expect(user.errors.full_messages).to eq([])
+  context 'emailがnilのとき' do
+    let(:user) {build(:user, email:nil)}
+    example 'errorsに「email列が空であるエラー」が格納される' do
+      user.valid?
+      expect(user.errors).to be_of_kind(:email, :blank)
+    end
+  end
+
+  context 'passwordがnilのとき' do
+    let(:user) {build(:user, password:nil)}
+    example 'errorsに「password列が空であるエラー」が格納される' do
+      user.valid?
+      expect(user.errors).to be_of_kind(:password, :blank)
+    end
   end
 end
