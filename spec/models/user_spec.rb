646d4bf53c4ae5f00fc2e13c36ee2463774ae95e
Yeah-metcha-Someday <itowokashigenari.jobjobworking@gmail.com>
2024-04-25 15:14:10 +0900

    add:created_at, updated_atカラムを追加した test:複数のexpectに対しaggregate_failuresを追加した

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,31 +1,39 @@
 RSpec.describe User, type: :model do
-  context 'emailとpasswordがそれぞれ有効な値であるとき' do
-    let(:user) { build(:user) }
+  describe '#validation' do
+    context 'emailとpasswordがそれぞれ有効な値であるとき' do
+      let(:user) { build(:user) }
 
-    it 'userが有効であり、errorsが空である' do
-      user.valid?
-      expect(user.valid?).to be true
-      expect(user.errors.full_messages).to eq([])
+      it 'userが有効であり、errorsが空である' do
+        aggregate_failures do
+          user.valid?
+          expect(user.valid?).to be true
+          expect(user.errors.full_messages).to eq([])
+        end
+      end
     end
-  end
 
-  context 'emailがnilのとき' do
-    let(:user) {build(:user, email:nil)}
+    context 'emailがnilのとき' do
+      let(:user) {build(:user, email:nil)}
 
-    it 'errorsに「email列が空であるエラー」が格納される' do
-      user.valid?
-      expect(user.errors).to be_of_kind(:email, :blank)
-      expect(user.errors.full_messages_for(:email)).to eq(["Email can't be blank"])
+      it 'errorsに「email列が空であるエラー」が格納される' do
+        aggregate_failures do
+          user.valid?
+          expect(user.errors).to be_of_kind(:email, :blank)
+          expect(user.errors.full_messages_for(:email)).to eq(["Email can't be blank"])
+        end
+      end
     end
-  end
 
-  context 'passwordがnilのとき' do
-    let(:user) {build(:user, password:nil)}
+    context 'passwordがnilのとき' do
+      let(:user) {build(:user, password:nil)}
 
-    it 'errorsに「password列が空であるエラー」が格納される' do
-      user.valid?
-      expect(user.errors).to be_of_kind(:password, :blank)
-      expect(user.errors.full_messages_for(:password)).to eq(["Password can't be blank"])
+      it 'errorsに「password列が空であるエラー」が格納される' do
+        aggregate_failures do
+          user.valid?
+          expect(user.errors).to be_of_kind(:password, :blank)
+          expect(user.errors.full_messages_for(:password)).to eq(["Password can't be blank"])
+        end
+      end
     end
   end
 end
