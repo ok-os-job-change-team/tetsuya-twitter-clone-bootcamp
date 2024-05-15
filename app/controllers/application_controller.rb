bd99bf3eda53a7ebdbcf1c9182be7a70a0f9098e
Yeah-metcha-Someday <itowokashigenari.jobjobworking@gmail.com>
2024-05-15 19:32:17 +0900

    refactor: 簡単のため、404エラー以外の記述を削除した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,11 @@
 # 404ページと500ページ
 class ApplicationController < ActionController::Base
-  unless Rails.env.production? # development?
-    rescue_from Exception, with: :render500
-    rescue_from ActiveRecord::RecordNotFound, with: :render404
-    rescue_from ActionController::RoutingError, with: :render404
-  end
-
-  def routing_error
-    raise ActionController::RoutingError, params[:path]
-  end
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActionController::RoutingError, with: :render_not_found
 
   private
 
-  def render404
+  def render_not_found
     render 'errors/404', status: :not_found
-  end
-
-  def render500
-    render 'errors/500', status: :internal_server_error
   end
 end
