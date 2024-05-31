d6e9e1230cdf0fa2f4d250cf543ab0717c0d8cc0
Yeah-metcha-Someday <itowokashigenari.jobjobworking@gmail.com>
2024-05-31 16:23:03 +0900

    feat: 新規投稿機能を実装した

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :check_logged_in, only: %i[index show]
+  before_action :check_logged_in, only: %i[index show new create]
 
   # GET /posts
   def index
@@ -10,5 +10,25 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @user = @post.user
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_url, notice: '投稿しました'
+    else
+      flash.now[:alert] = '投稿に失敗しました'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content).merge(user_id: current_user.id)
   end
 end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -60,4 +60,69 @@ RSpec.describe PostsController, type: :request do
       end
     end
   end
+
+  describe 'GET /posts/new' do
+    context 'ログイン状態で新規投稿画面にアクセスするとき' do
+      include_context 'userでログインする'
+
+      it 'アクセスに成功する' do
+        aggregate_failures do
+          get new_post_path
+          expect(response).to have_http_status(:success)
+          expect(response.body).to include '新規投稿'
+        end
+      end
+    end
+
+    context 'ログインしないで新規投稿画面にアクセスするとき' do
+      it 'ログインページにリダイレクトされる' do
+        aggregate_failures do
+          get new_post_path
+          expect(response).to redirect_to(login_url)
+          expect(flash[:alert]).to eq('ログインしてください')
+        end
+      end
+    end
+  end
+
+  describe 'POST /posts' do
+    context 'ログイン状態で新規投稿するとき' do
+      include_context 'userでログインする'
+
+      context '投稿のパラメータが有効であるとき' do
+        it '投稿に成功し、投稿一覧画面にリダイレクトされる' do
+          aggregate_failures do
+            expect do
+              post posts_path, params: { post: attributes_for(:post, user_id: user.id) }
+            end.to change(Post, :count).by(1)
+            expect(response).to redirect_to posts_url
+          end
+        end
+      end
+
+      context 'contentがnilであるとき' do
+        it '投稿に失敗し、新規投稿画面がリロードされる' do
+          aggregate_failures do
+            expect do
+              post posts_path, params: { post: attributes_for(:post, content: nil, user_id: user.id) }
+            end.not_to change(Post, :count)
+            expect(response.body).to include '新規投稿'
+            expect(flash[:alert]).to eq('投稿に失敗しました')
+          end
+        end
+      end
+    end
+
+    context 'ログインしないで新規投稿するとき' do
+      it '投稿に失敗し、ログイン画面にリダイレクトされる' do
+        aggregate_failures do
+          expect do
+            post posts_path, params: { post: attributes_for(:post, user_id: user.id) }
+          end.not_to change(Post, :count)
+          expect(response).to redirect_to(login_url)
+          expect(flash[:alert]).to eq('ログインしてください')
+        end
+      end
+    end
+  end
 end
